899b7ca594569f2aca7bc3ef9bee25d3c330b146
lea-bramoulle <l.bramoulle@hotmail.fr>
2022-06-16 23:07:17 +0200

    :rocket: feat : display form data on submit

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,35 @@
 // == Import
 import './styles.scss';
 
+import { useState } from 'react';
+
 import Map from '../Map';
 import Header from '../Header';
 import Main from '../Main';
 
 // == Composant
 function App() {
+  const [inputValue, setInputValue] = useState([]);
+  const [IpAddress, setIpAddress] = useState('ze');
+
+  const handleFormInputChange = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setIpAddress(inputValue);
+    setInputValue('');
+  };
+
   return (
     <div className="app">
-      <Header />
+      <Header 
+        onFormSubmit={handleFormSubmit}
+        onInputValueChange={handleFormInputChange}
+        ipAddress={IpAddress}
+      />
       <div>
         <div className="z-index2">
           <Main />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
+import PropTypes from 'prop-types';
 import arrowButtonSvg from '../../assets/images/icon-arrow.svg';
 
 import './header.scss';
 
-function Header() {
+function Header({ ipAddress, onFormSubmit, onInputValueChange }) {
   return (
     <header className="header">
-      <h1 className="header--title">IP AddressTracker</h1>
-      <form className="form">
+      <h1 className="header--title">IP AddressTracker {ipAddress}</h1>
+      <form className="form" onSubmit={onFormSubmit}>
         <input
           type="text"
+          name="search"
           placeholder="Search for any IP address or domain"
           className="form--input"
+          onChange={onInputValueChange}
         />
         <button type="submit" className="form--button">
           <img src={arrowButtonSvg} alt="submit button icon" />
@@ -19,5 +22,11 @@ function Header() {
     </header>
   );
 }
+
+Header.propTypes = {
+  onFormSubmit: PropTypes.func.isRequired,
+  onInputValueChange: PropTypes.func.isRequired,
+  ipAddress: PropTypes.string.isRequired,
+};
 
 export default Header;
