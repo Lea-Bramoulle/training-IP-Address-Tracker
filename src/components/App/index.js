314d44310d76f8f3b027ccc3ed2d4b19f9c336b4
lea-bramoulle <l.bramoulle@hotmail.fr>
2022-06-21 18:15:30 +0200

    :rocket: feat : display and update Map component  after Ip research

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // == Import
 import './styles.scss';
 
@@ -16,45 +17,44 @@ function App() {
   const [ipAddressInformations, setIpAddressInformations] = useState({});
   const [geoCode, setGeoCode] = useState([]);
 
-  const loadApiAddress = async function () {
-    $.getJSON('https://jsonip.com?callback=?', (data) => {
-      setIpAddress(data.ip);
-    });
-  };
-
-  const geoCoder = async function () {
-    const ipData = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_GEO_IPIFY_API_KEY}&ipAddress=${ipAddress}`);
-
+  const geoCoder = async function (data) {
     try {
       const provider = new OpenStreetMapProvider();
-
-      const results = await provider.search({ query: `${ipData.data.location.region}, ${ipData.data.location.country}` });
+      const results = await provider.search({ query: `${data.location.region}, ${data.location.country}` });
 
       console.log([results[0].y, results[0].x]);
-      const newTab = [];
-      newTab.push(results[0].y, results[0].x);
-      setGeoCode(newTab);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const loadApiData = async function () {
-    try {
-      setInputValue('');
-      const ipAddressData = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_GEO_IPIFY_API_KEY}&ipAddress=${ipAddress}`);
-
-      console.log(ipAddressData.data);
-
-      setIpAddressInformations(ipAddressData.data);
-      geoCoder();
+      setGeoCode([results[0].y, results[0].x]);
     }
     catch (error) {
       console.error(error);
     }
   };
 
+  // const loadApiAddress = async function () {
+  //   $.getJSON('https://jsonip.com?callback=?', (data) => {
+  //     setIpAddress(data.ip);
+  //   });
+  // };
 
+  const fetchIpDataFromApi = async (ipAddress) => {
+    try {
+      const ipAddressData = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_GEO_IPIFY_API_KEY}&ipAddress=${ipAddress}`);
+
+      setIpAddressInformations(ipAddressData.data);
+      console.log(ipAddressInformations);
+      setGeoCode([]);
+
+      geoCoder(ipAddressData.data);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loadApiData = async function () {
+    setInputValue('');
+    fetchIpDataFromApi(ipAddress);
+  };
 
   const handleFormInputChange = (event) => {
     setInputValue(event.target.value);
@@ -63,23 +63,11 @@ function App() {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      setIpAddress(inputValue);
-      setInputValue('');
-      const ipAddressData = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_GEO_IPIFY_API_KEY}&ipAddress=${ipAddress}`);
+    setIpAddress(inputValue);
+    setInputValue('');
 
-      setIpAddressInformations(ipAddressData.data);
-      console.log(ipAddressInformations);
-      geoCoder();
-    }
-    catch (error) {
-      console.error(error);
-    }
+    fetchIpDataFromApi(inputValue);
   };
-
-  useEffect(() => {
-    loadApiAddress();
-  }, []);
 
   useEffect(() => {
     loadApiData();
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,11 +1,12 @@
 import './map.scss';
 
+import PropTypes from 'prop-types';
 import { MapContainer, TileLayer } from 'react-leaflet';
 
 function Map({ geoCodeInformations }) {
   return (
     <div>
-      <MapContainer className="map-container" center={[geoCodeInformations[0], geoCodeInformations[1]]} zoom={8} scrollWheelZoom>
+      <MapContainer className="map-container" center={geoCodeInformations} zoom={8} scrollWheelZoom>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -15,14 +16,8 @@ function Map({ geoCodeInformations }) {
   );
 }
 
+Map.propTypes = {
+  geoCodeInformations: PropTypes.array.isRequired,
+};
+
 export default Map;
-
-// const Geocoder = async () => {
-//   const provider = new OpenStreetMapProvider();
-
-//   const results = await provider.search({ query: 'Auvergne-Rhône-Alpes, FR' });
-
-//   console.log([results[0].x, results[0].y]);
-// };
-
-// Geocoder();
